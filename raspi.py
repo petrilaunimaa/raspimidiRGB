afe935ce5635ecafa7bce4e11ac7784adbe22a08
Petri Launimaa <launimaa.petri@gmail.com>
2021-11-12 00:54:23 +0000

    working example debugged

--- raspi.py
+++ raspi.py
@@ -8,15 +8,40 @@ import neopixel
 pixels = neopixel.NeoPixel(board.D18, 300)
 
 port = mido.open_input('f_midi')  # open USB port
+red = 0.0
+green = 0.0
+blue = 0.0
 while True:
-    try:  # This filters out all non-note data
-        for msg in port.iter_pending():  # if there is a message pending
-            if msg.type == 'pitchwheel':  # of type pitchwheel
-                print("PITCH: ", msg.pitch)
-                out = interp(msg.pitch, [-8192, 8192], [0, 255])
-                pixels.fill((0, out, 0))
-                if msg.type == 'control_change' and msg.control == 1:
-                    print("MOD: ", msg.value)
+    try:
+        for msg in port.iter_pending():
+            if(msg.type == 'note_on'):
+                out = interp(msg.velocity, [0,127],[0,255])
+                if(msg.note == 60):
+                    red = out
+                if(msg.note == 61):
+                    green = out
+                if(msg.note == 62):
+                    blue = out
+                if(msg.note == 65):
+                    red = out
+                    green = out
+                    blue = out
+            if(msg.type == 'note_off'):
+                if(msg.note == 60):
+                    red = 0
+                if(msg.note == 61):
+                    green = 0
+                if(msg.note == 62):
+                    blue = 0
+                if(msg.note == 65):
+                    red = 0
+                    green = 0
+                    blue = 0
+            if msg.type == 'control_change' and msg.control == 1:
+                red = red * msg.value
+                green = green * msg.value
+                blue = blue * msg.value
+            pixels.fill((red/2, green/2, blue/2))
     except AttributeError as error:
         print("Error excepted")
     pass
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install libasound2-dev libjack0 libjack-dev python3-pip
-sudo pip3 install mido python-rtmidi rpi_ws281x adafruit-circuitpython-neopixel
+sudo pip3 install mido python-rtmidi rpi_ws281x adafruit-circuitpython-neopixel python3-numpy
